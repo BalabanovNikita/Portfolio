e58b0b76d8a7e9a71e3bf0a3244c32f81e84d518
NikitaBalabanov16 <88787633+NikitaBalabanov16@users.noreply.github.com>
2021-08-17 20:28:31 +0300

    Snake
    Snake game

---
+++ Snake/Snake/Snake/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80,27);
+            Console.SetBufferSize(80,27);
+
+
+
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+            FoodCreater foodCreater = new FoodCreater(80,25, '$');
+            Point food = foodCreater.CreateFood();
+            food.Draw();
+            Point p1 = new Point(5, 3, '*');
+            Snake snake = new Snake(p1, 4, Direction.RIGHT);
+            snake.Draw();
+            while (true)
+            {
+                if (walls.IsHIt(snake) || snake.IsHitTail())
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.SetCursorPosition(80/2-5, 25/2);
+                    Console.WriteLine("Game over!");
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreater.CreateFood();
+                    food.Draw();
+                }
+                else snake.Move();
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(200);
+
+            }
+            Console.ReadLine();
+        }
+        static void Draw(Figure figure)
+        {
+            figure.Draw();
+        }
+        
+    }
+}
---
+++ Snake/Snake/Snake/Snake.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake : Figure
+    {
+        public int eatCount;
+        public Direction direction;
+        public Snake(Point tail, int lenght, Direction _direction)
+        {
+            eatCount = 0;
+            direction = _direction;
+            pList = new List<Point>();
+            for (int i = 0; i < lenght; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+ 
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+            Console.SetCursorPosition(3, 26);
+            Console.Write("Еды съедено: " + eatCount);
+        }
+
+        internal bool IsHitTail()
+        {
+            var head = pList.Last();
+            for (int i = 0; i<pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[i])) return true;
+            }
+            return false;
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
+        }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow) direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow) direction = Direction.RIGHT;
+            else if (key == ConsoleKey.DownArrow) direction = Direction.DOWN;
+            else if (key == ConsoleKey.UpArrow) direction = Direction.UP;
+
+        }
+
+
+        public bool Eat (Point food)
+        {
+            Point head = GetNextPoint();
+            if (head.IsHit(food))
+            {
+                eatCount++;
+                food.sym = head.sym;
+                food.Draw();
+                pList.Add(food);
+                return true;
+            }
+            else return false;
+        }
+    }
+}
